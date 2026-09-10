608cce4b30c7f20f3434fe5024510e7281428e06
燃,. <randaoshengxia@gmail.com>
2026-09-10 19:12:36 +0800

    feat: 项目更名

--- packages/core/layouts/default/header/logo.tsx
+++ packages/core/layouts/default/header/logo.tsx
@@ -7,8 +7,8 @@ export const Logo = defineComponent({
       <div class="w-420px h-full relative">
         <img src={logoBgImg} alt="" class="h-full absolute z-0 object-fill brightness-60" />
 
-        <div class="absolute left-24px top-1/2 -translate-y-1/2 text-28px font-900 text-white whitespace-nowrap z-50">
-          武汉市更新综合管理平台
+        <div class="absolute left-24px top-1/2 -translate-y-1/2 text-24px font-900 text-white whitespace-nowrap z-50">
+          {import.meta.env.VITE_GLOB_APP_TITLE}
         </div>
       </div>
     );
